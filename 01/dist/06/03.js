572a261eafef9472c5c15662fb43a0622558cd75
Maria Baykova <mnbaikova@gmail.com>
2023-11-09 21:13:31 +0500

    06d 03.ts + recursion

--- 01/dist/06/03.js
+++ 01/dist/06/03.js
@@ -5,6 +5,8 @@ const arr1 = [[1, 2], [[1]], 2]; // [1, 2, [1], 2]
 const arr2 = [[3, 4], [[3]], 4]; // [ 3, 4, 3, 4]
 console.log(flat(arr1));
 console.log(flat(arr2, 2));
+console.log(flatRec(arr1));
+console.log(flatRec(arr2, 2));
 function flat(array, depth) {
     if (!depth) {
         depth = 1;
@@ -27,5 +29,23 @@ function flat(array, depth) {
         step++;
     }
     return stack;
+}
+function flatRec(arr, depth = 1) {
+    let result = [];
+    if (depth <= 0) {
+        return [...arr];
+    }
+    const arrLength = arr.length;
+    for (let i = 0; i < arrLength; i++) {
+        // console.log(i + " " + result);
+        if (Array.isArray(arr[i])) {
+            result.push(...flatRec([...arr[i]], depth - 1));
+        }
+        else {
+            result.push(...flatRec([arr[i]], depth - 1));
+        }
+        // console.log(i + " " + result);
+    }
+    return result;
 }
 //# sourceMappingURL=03.js.map
--- 01/src/06/03.ts
+++ 01/src/06/03.ts
@@ -8,6 +8,9 @@ const arr2 = [ [3,4], [[3]], 4 ]; // [ 3, 4, 3, 4]
 console.log( flat(arr1) );
 console.log( flat(arr2, 2) );
 
+console.log( flatRec(arr1) );
+console.log( flatRec(arr2, 2) );
+
 function flat( array, depth? ) {
     if ( !depth ) {
         depth = 1;
@@ -32,4 +35,24 @@ function flat( array, depth? ) {
 
 
     return stack;
+}
+
+
+function flatRec( arr, depth = 1 ) {
+    let result = [];
+    if ( depth <= 0 ) {
+        return [...arr];
+    }
+    const arrLength = arr.length;
+    for ( let i = 0; i < arrLength; i++ ) {
+        // console.log(i + " " + result);
+        if ( Array.isArray(arr[i]) ) {
+            result.push(...flatRec( [...arr[i]], depth - 1 ));
+        } else {
+            result.push(...flatRec( [arr[i]], depth - 1 ));
+        }
+        // console.log(i + " " + result);
+    }
+
+    return result;
 }
