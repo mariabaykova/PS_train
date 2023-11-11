52de02cf39cb6226a1ee150f37a35bee4a8496f3
Maria Baykova <mnbaikova@gmail.com>
2023-11-11 16:01:17 +0500

    07d 03.ts new polished ver

--- 01/dist/07/03.js
+++ 01/dist/07/03.js
@@ -1,8 +1,18 @@
 // преобразование camel case --> dash-style
 console.log(dasherize("createDocumentFragment"));
-console.log(dasherize("PUPERsuperMAN"));
+console.log(dasherize("PUPERSuperMAN"));
 console.log(dasherize("VirtualDOMFragment"));
+console.log(dasherize("VirtualDOMFragmentDOMFragment"));
+console.log(dasherize("Some123VALUE10"));
+console.log(dasherize("SOME123VALUE10"));
+console.log(dasherize("123VALUE10"));
+console.log(dasherize("S123VALUE10"));
 function dasherize(str) {
-    return str.replace(/([A-Z]+?)/g, (_, p1) => `-${p1.toLowerCase()}`).replace(/^(-)/, "");
+    return str
+        .replace(/([0-9]{1,})/g, "$1-") // "-" после всех групп цифр 
+        .replace(/([a-z])([A-Z])/g, "$1-$2") // "-" между маленькой и большой везде
+        .replace(/([A-Z]{1,})([A-Z]{1})([a-z$])/g, "$1-$2$3") // "-" между двумя большими, если следом маленькая
+        .replace(/(-)$/, "") // убрать "-" в конце строки
+        .toLowerCase();
 }
 //# sourceMappingURL=03.js.map
