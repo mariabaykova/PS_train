8fb801b7bf450f0be737f58cefd87bb266cc84c8
Maria Baykova <mnbaikova@gmail.com>
2024-01-17 10:53:56 +0500

    -

---
+++ 01/dist/addTasks/iter/rec-iter.js
@@ -0,0 +1,32 @@
+// есть рекурсивная структура - бинарное дерево
+// задача - обойти все его листья c помощью итератора
+// из лекции N 16 продвинутые итераторы
+class TNode {
+    value;
+    left;
+    right;
+    constructor(value, { left, right } = {}) {
+        this.value = value;
+        this.left = left ?? null;
+        this.right = right ?? null;
+    }
+    // constructor(value: T, left?: TNode<T> | null, right?: TNode<T> | null) {
+    //     this.value = value;
+    //     this.left = left ?? null;
+    //     this.right = right ?? null;
+    // }
+    [Symbol.iterator]() {
+        let state = 'current';
+        return {
+            [Symbol.iterator]() {
+                return this;
+            },
+            next: () => {
+            }
+        };
+    }
+}
+const n = new TNode(2, { left: new TNode(3, { right: new TNode(5, { left: new TNode(55), right: new TNode(56) }) }), right: new TNode(4) });
+// const n = new TNode( 2, new TNode(3), new TNode(4) );
+console.log(n);
+//# sourceMappingURL=rec-iter.js.map
